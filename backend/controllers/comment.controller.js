b9aa54ce588a2bed1e144f66b12a60f1d00a0582
Aman Ahamed <amanahamed04@gmail.com>
2025-12-28 17:42:40 +0530

    create comment controller

---
+++ backend/controllers/comment.controller.js
@@ -0,0 +1,55 @@
+import { Blog } from "../models/blog.model.js";
+import { Comment } from "../models/comment.modal.js";
+
+export const createComment = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const commentUserId = req.id;
+    const { content } = req.body;
+
+    const blog = await Blog.findById(postId);
+
+    if (!blog) {
+      return res.status(400).json({
+        success: false,
+        message: "Blog Not found",
+      });
+    }
+
+    if (!content) {
+      return res.status(400).json({
+        success: false,
+        message: "Text is required",
+      });
+    }
+
+    const comment = await Comment.create({
+      content,
+      userId: commentUserId,
+      postId,
+    });
+
+    await comment.populate({
+      path: "userId",
+      select: "firstName lastName photoUrl",
+    });
+
+    blog.comments.push(comment._id);
+
+    await blog.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "Comment Added",
+      comment,
+    });
+    
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to Create Comment",
+      error: error.message,
+    });
+  }
+};
